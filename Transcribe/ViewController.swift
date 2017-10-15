37532cc8bc07b1360b6be4c1aab7d5bdd19311d8
V-Kwong <vincent_kwong578@hotmail.com>
2017-10-15 12:57:55 -0700

    Minimal Realtime Speech to Text

--- Transcribe/ViewController.swift
+++ Transcribe/ViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import Speech
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
+    
+    @IBOutlet weak var SpeechToTextLabel: UILabel!
+    @IBOutlet weak var StartStopButton: UIButton!
+    
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer()
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,40 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        self.recordAndRecognizeSpeech()
+    }
+    
+    func recordAndRecognizeSpeech() {
+        let node = audioEngine.inputNode
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+            self.request.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        
+        guard let myRecognizer = SFSpeechRecognizer() else { return }
+        if !myRecognizer.isAvailable {
+            return
+        }
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+                let bestString = result.bestTranscription.formattedString
+                self.SpeechToTextLabel.text = bestString
+                
+            } else if let error = error {
+                print(error)
+            }
+        })
+    }
 
 }
 
